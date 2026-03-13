2d1027574d6ab4df011fba4f80bfd351fd5a9f73
jiyu <pllpopogg@gmail.com>
2026-03-13 21:21:20 +0900

    4-3

--- code/4-3.c
+++ code/4-3.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+#pragma warning(disable:4996)
 
 int main()
 {
+    int num;
+    int isPrime = 1; //true
+
+    scanf("%d", &num);
+
+    if (num == 1) {
+        isPrime = 0;
+    }
+    else {
+        for (int i = 2; i <= num/2 ; i++) {
+            if (num % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+
+    if (isPrime == 1) {
+        printf("true");
+    }
+    else {
+        printf("false");
+    }
 
     return 0;
 }
-

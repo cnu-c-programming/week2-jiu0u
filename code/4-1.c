7d6c7a807625ce967dbf950b742972a7db33d39b
jiyu <pllpopogg@gmail.com>
2026-03-13 16:06:38 +0900

    4-1

--- code/4-1.c
+++ code/4-1.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
+#pragma warning(disable:4996)
 
 int main()
 {
+    int a, b, result;
+    char op;
+
+    scanf("%d %d %c", &a, &b, &op);
+
+    switch (op) {
+    case '-':
+        result = a - b;
+        break;
+    case '+':
+        result = a + b;
+        break;
+    case '/':
+        result = a / b;
+        break;
+    case '*':
+        result = a * b;
+        break;
+    }
+
+    printf("%d", result);
 
     return 0;
 }
-

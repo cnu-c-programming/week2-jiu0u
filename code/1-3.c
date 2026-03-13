a5dbe75a15e50284866f2cb48cf51116c31fb5ff
jiyu <pllpopogg@gmail.com>
2026-03-13 09:53:46 +0900

    1-3

--- code/1-3.c
+++ code/1-3.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#pragma warning(disable:4996)
 
 int main()
 {
+    int age;
+    char grade;
+    float gpa;
 
+    scanf("%d %c %f", &age, &grade, &gpa);
+    printf("%d %c %f\n", age, grade, gpa);
 
     return 0;
 }

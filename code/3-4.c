b62380bb327be32be2d8857b95aff546e9a8f60c
jiyu <pllpopogg@gmail.com>
2026-03-15 14:17:46 +0900

    3-4*
    Add variable assignments and print statements to demonstrate operations.

--- code/3-4.c
+++ code/3-4.c
@@ -2,7 +2,16 @@
 
 int main()
 {
+    int a, b, c;
+
+    a = b = c = 5;
+    printf("%d %d %d\n", a, b, c); //5 5 5
+
+    a = 2 + 3 * 4;
+    printf("%d %d %d\n", a, b, c); //14 5 5
+
+    c = a++ + ++b; // 14 + 6
+    printf("%d %d %d\n", a, b, c); // 15 6 20 > 15 6 5?
 
     return 0;
 }
-
